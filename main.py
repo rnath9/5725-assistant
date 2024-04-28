aa2351d7f64c17047ac80ab0c25e00fb9a9127ce
Rahul <rrn9@cornell.edu>
2024-04-28 03:55:09 -0400

    Implemented rook logic
    - Implemented the basis for the rook logic, briefly tested for it in main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from board_cell import Square
 from piece import Piece
 from pawn import Pawn
+from rook import Rook
 
 #TODO turn that into a package
 
@@ -8,6 +9,10 @@ board = [[Square() for _ in range(8)] for _ in range(8)]
 for col in range(8):
   board[1][col].piece = Pawn("white", [1, col])
   board[6][col].piece = Pawn("black", [6, col])
+
+for r in [0,7]:
+  for c in [0,7]:
+    board[r][c].piece = Rook("white" if r == 0 else "black", [r,c])
 
 """Prints ths square name of a coordinate (ex: [0,0] maps to A1)"""
 def print_cell(coordinates):
@@ -21,16 +26,33 @@ def get_coordinates(square_name):
 
 print("verify valid board")
 print(board)
-print("\n checking white E pawn's moves")
+print("\nchecking white E pawn's moves")
 E2 = get_coordinates("E2")
 white_e_pawn = board[E2[0]][E2[1]].piece
 
 for val in white_e_pawn.available_moves(board):
   print_cell(val)
 
-print("\n checking black E pawn's moves")
+print("\nchecking black E pawn's moves")
 E7 = get_coordinates("E7")
 black_e_pawn = board[E7[0]][E7[1]].piece
 
 for val in black_e_pawn.available_moves(board):
+  print_cell(val)
+
+
+# print("\nrook A1's moves")
+# A1 = get_coordinates("A1")
+# rook = board[A1[0]][A1[1]].piece
+
+
+# for val in rook.available_moves(board):
+#   print_cell(val)
+
+# print(rook.available_moves(board))
+
+print("\ncreating a dummy black rook at A3, testing moves\n")
+board[2][0].piece = Rook("black", [2,0])
+rookA3 = board[2][0].piece
+for val in rookA3.available_moves(board):
   print_cell(val)
---
+++ rook.py
@@ -0,0 +1,49 @@
+from piece import Piece
+
+class Rook(Piece):
+  def __init__(self, color, position):
+    super().__init__(color, position)
+
+  def __str__(self):
+    return "r"
+
+  def __repr__(self):
+    return self.__str__()
+  def available_moves(self, board):
+    res = []
+    #check if can move up one or two
+    #print("row logic")
+    possible = [range(-1, -8, -1),range(1,8)]
+    for ranges in possible:
+      for dr in ranges:
+        if self.row + dr <0 or self.row + dr >7:
+          #print(str(self.row+dr) + " out of bounds, breaking")
+          break #check if out of bounds
+        if board[self.row + dr][self.col].piece == None:
+          res.append([self.row + dr, self.col])
+        #piece present, can we capture it?
+        if board[self.row + dr][self.col].piece != None:
+          if board[self.row + dr][self.col].piece.color != self.color:
+            #print("takeable piece at " + str(self.row + dr) + "," + str(self.col))
+            res.append([self.row + dr, self.col]) # can take
+          #else:
+            #print("not takeable piece at " + str(self.row + dr) + "," + str(self.col))
+          break
+    #same logic but for the columns
+    #print("cols logic")
+    for ranges in possible:
+      for dc in ranges:
+        if self.col + dc <0 or self.col + dc >7:
+          #print(str(self.col+dc) + " out of bounds, breaking")
+          break #check if out of bounds
+        if board[self.row][self.col+dc].piece == None:
+          res.append([self.row, self.col+dc])
+        #piece present
+        if board[self.row ][self.col+dc].piece != None:
+          if board[self.row ][self.col+dc].piece.color != self.color:
+            #print("takeable piece at " + str(self.row) + "," + str(self.col+dc))
+            res.append([self.row, dc+ self.col]) # can take
+          #else:
+            #print("not takeable piece at " + str(self.row) + "," + str(self.col+dc))
+          break
+    return res
