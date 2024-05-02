4bb5a72f392f4134e8dcdbdb3e4d5a9d0ba325f8
Wak3en <joaquindiegorivera@gmail.com>
2024-05-02 15:43:16 -0400

    Completed Weather API calling

--- weather.py
+++ weather.py
@@ -10,7 +10,7 @@ numberMap = {"one":1, "two":2 ,"three":3, "four":4, "five":5, "six":6,"seven":7,
 
 weather = get_weather()
 
-def current_day(forecast):
+def current_day():
     index = datetime.datetime.now().weekday()
     return weekdays[index]
 
@@ -23,14 +23,14 @@ def parse_sentence_for_time(s):
         period = "am"
     else:
         period = "pm"
-    for index in range(len(tokens) - 1):
+    for index in range(len(tokens)):
         if tokens[index] in weekdays:
             day = tokens[index]
         if tokens[index] in numberMap and time == "":
             time = tokens[index]
 
     if day is None:
-        day = current_day(weather)
+        day = current_day()
     if time == "":
         if "night" in tokens or "tonight" in tokens:
             time = "eight"
@@ -64,7 +64,33 @@ def get_weather_results(input):
     day_time = parse_sentence_for_time(input)
     if day_time == -1:
         raise ValueError
-    day, time = day_time
-    today = current_day(weather)
-    print(day)
-    print(time)
+    day, time  = day_time
+    w = get_weather()
+    index = find_index(day,time)
+    return w['properties']['periods'][index]['name'], w['properties']['periods'][index]['detailedForecast']
+
+def find_index(day,time):
+    time_bonus = 0
+    if (time <= 4):
+        time_bonus = -1
+    elif (time >= 18):
+        time_bonus = 1
+    else: 
+        time_bonus = 0
+    curr_time_offset = 0
+    if (datetime.datetime.now().hour >= 18):
+        curr_time_offset = -1
+    today = current_day()
+    today_index = weekdays.index(today)
+    day_index = weekdays.index(day)
+    diff = day_index - today_index
+    result = 0
+    if (diff <0):
+        result = (7 + diff) * 2 + time_bonus + curr_time_offset
+    else:
+        result = diff * 2 + time_bonus + curr_time_offset
+
+    if (result < 0 or result > 14):
+        result = 0
+
+    return result
